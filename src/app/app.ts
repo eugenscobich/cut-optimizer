cc4abc58b75f5f1d4513c349c9a9382859650aae
Evgheni Scobici <evghenii.scobici@endava.com>
2026-03-10 16:05:59 +0200

    next commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { Component, signal, Input } from '@angular/core';
+import { Component, Type, signal, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DockviewAngularModule } from 'dockview-angular';
-import { DockviewReadyEvent, DockviewPanelApi } from 'dockview-core';
 
 // Default panel component
 @Component({
@@ -20,14 +19,13 @@ export class DefaultPanelComponent {
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [DockviewAngularModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('cut-optimizer');
-  protected readonly components: Record<string, any> = {};
+  components: Record<string, any> = {};
 
   constructor() {
     this.components = {
@@ -35,7 +33,7 @@ export class App {
     };
   }
 
-  onReady(event: DockviewReadyEvent) {
+  onReady(event: any) {
     event.api.addPanel({
       id: 'panel_1',
       component: 'default',
